42473950508036943074a1db94a73d78b35a36b5
Krosmez <jonathanmaison78@gmail.com>
2023-10-10 12:51:15 -0400

    test

--- assets/js/pages/home.js
+++ assets/js/pages/home.js
@@ -1,13 +1,16 @@
 function filterPosts() {
-  let selectPosts = document.getElementById('select-posts');
-  let entries = document.querySelectorAll('.post-entry-filter');
+  let selectPosts = document.getElementById("select-posts");
+  let entries = document.querySelectorAll(".post-entry-filter");
   if (selectPosts) {
-    selectPosts.addEventListener('change', () => {
-      entries.forEach(function(entry) {
-        if (entry.classList.contains(`entry--${selectPosts.value}`) | selectPosts.value === 'all-posts') {
-          entry.style.display = 'block';
+    selectPosts.addEventListener("change", () => {
+      entries.forEach(function (entry) {
+        if (
+          entry.classList.contains(`entry--${selectPosts.value}`) |
+          (selectPosts.value === "all-posts")
+        ) {
+          entry.style.display = "block";
         } else {
-          entry.style.display = 'none';
+          entry.style.display = "none";
         }
       });
     });
