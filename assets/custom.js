b55a51c7afa7a809ee52d30c0755b99e1d2fd71a
Krosmez <jonathanmaison78@gmail.com>
2023-10-10 13:56:30 -0400

    [SCSS] Made small general style implementation

--- assets/custom.js
+++ assets/custom.js
@@ -1,14 +1,17 @@
 // Add custom JS
 
+// Trouble with className not found
+// So i'm going to comment that until a solution is find
 
-var slider_left = document.getElementsByClassName("slider_left")[0];
-var slider_right = document.getElementsByClassName("slider_right")[0];
-var carousel = document.getElementsByClassName("carousel")[0];
+// const slider_left = document.getElementsByClassName("slider_left")[0];
+// const slider_right = document.getElementsByClassName("slider_right")[0];
+// const carousel = document.getElementsByClassName("carousel")[0];
 
-    slider_left.addEventListener("click", function(e){
-        console.log(e.target)
-        carousel.style.overflow = "-200px";
-    })
-    slider_right.addEventListener("click", function(e){
-        console.log(e.target)
-    })
+// slider_left.addEventListener("click", function (e) {
+//   console.log(e.target);
+//   carousel.style.overflow = "-200px";
+// });
+
+// slider_right.addEventListener("click", function (e) {
+//   console.log(e.target);
+// });
