b1be4c3b98eb29da75a40e6e2e4784b821a955b8
Martin Gasque <mgasque.pro@gmail.com>
2023-10-12 18:31:59 +0200

    add npm

--- tina/config.ts
+++ tina/config.ts
@@ -1,21 +1,20 @@
 import { defineConfig } from 'tinacms';
 
 // Your hosting provider likely exposes this as an environment variable
-const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || 'master';
+const branch = 'master';
 
 export default defineConfig({
   branch,
-  clientId: '403e9ac8-0d3b-4d55-bfa2-e2acc1e6d028', // Get this from tina.io
-  token: '006256fba5595640e08c3a50d90a4ffd32789fbb', // Get this from tina.io
-
+  clientId: process.env.TINA_PUBLIC_CLIENT_ID!,
+  token: process.env.TINA_TOKEN,
   build: {
     outputFolder: 'admin',
-    publicFolder: 'assets',
+    publicFolder: 'static',
   },
   media: {
     tina: {
       mediaRoot: 'uploads',
-      publicFolder: 'assets',
+      publicFolder: 'static',
     },
   },
   schema: {
